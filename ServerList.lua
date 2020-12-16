817aa3b4fa715ad273762cbd6b6eb7473a9c04b8
stqcky <59932390+stqcky@users.noreply.github.com>
2020-12-16 11:35:55 +0300

    new eu east server

--- ServerList.lua
+++ ServerList.lua
@@ -1,4 +1,5 @@
 return {
     {Name = "South Asia #1", Address = "172.105.43.77"},
-    {Name = "US East #1", Address = "104.236.19.49"}
+    {Name = "US East #1", Address = "104.236.19.49"},
+    {Name = "EU East #1", Address = "46.146.163.21"}
 }
