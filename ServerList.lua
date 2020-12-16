3eb46de87370b604623558fe624d92456e2bcc8f
stqcky <59932390+stqcky@users.noreply.github.com>
2020-12-17 00:19:19 +0300

    new us midwest server & reorder

--- ServerList.lua
+++ ServerList.lua
@@ -1,5 +1,6 @@
 return {
-    {Name = "South Asia #1", Address = "172.105.43.77"},
     {Name = "US East #1", Address = "104.236.19.49"},
-    {Name = "EU East #1", Address = "46.146.163.21"}
+    {Name = "US MidWest #1", Address = "107.11.218.253"},
+    {Name = "EU East #1", Address = "46.146.163.21"},
+    {Name = "South Asia #1", Address = "172.105.43.77"}
 }
