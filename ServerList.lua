6b6c37200d5dc27292f96a6634a9e9ba7ad9f95d
stqcky <59932390+stqcky@users.noreply.github.com>
2021-03-30 19:01:48 +0300

    Update ServerList.lua

--- ServerList.lua
+++ ServerList.lua
@@ -1,6 +1,4 @@
 return {
     {Name = "US East #1", Address = "104.236.19.49"},
-    {Name = "US MidWest #1", Address = "107.11.218.253"},
-    {Name = "EU East #1", Address = "46.146.163.21"},
-    {Name = "South Asia #1", Address = "172.105.43.77"}
+    {Name = "EU East #1", Address = "46.146.163.21"}
 }
